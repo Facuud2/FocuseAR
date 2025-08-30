4b4006fcc6d1bb8d2603ee31c1c4e1e6af802e49
Facuud2 <lunariush553@gmail.com>
2025-08-30 17:02:03 -0300

    Descomentado LoginForm

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,28 +1,28 @@
-//Lo comenté ya que solo lo utilicé para comprobar que funcionaba el Login con Google
+// Lo comenté ya que solo lo utilicé para comprobar que funcionaba el Login con Google
 
-// import React from "react";
-// import { loginWithGoogle } from "./AuthService"; 
+import React from "react";
+import { loginWithGoogle } from "./AuthService"; 
 
-// const LoginForm: React.FC = () => { 
-//     const handleLogin = async () => { 
-//         try {
-//             await loginWithGoogle();
-//         } catch (error: any) {
-//             if (error.code !== 'auth/cancelled-popup-request') {
-//                 alert("Debes completar el inicio de sesión con Google"); 
-//             } else {
-//                 alert(error.message);
-//             }
+const LoginForm: React.FC = () => { 
+    const handleLogin = async () => { 
+        try {
+            await loginWithGoogle();
+        } catch (error: any) {
+            if (error.code !== 'auth/cancelled-popup-request') {
+                alert("Debes completar el inicio de sesión con Google"); 
+            } else {
+                alert(error.message);
+            }
 
-//         }
-//     };
+        }
+    };
 
-//     return (
-//         <div>
-//             <h2>Iniciar sesión</h2>
-//             <button onClick={handleLogin}>Iniciar sesión con Google</button>
-//         </div>
-//     );
-// };
+    return (
+        <div>
+            <h2>Iniciar sesión</h2>
+            <button onClick={handleLogin}>Iniciar sesión con Google</button>
+        </div>
+    );
+};
 
-// export default LoginForm; 
+export default LoginForm; 
