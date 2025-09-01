83ab3997346b0e023259b4437160effe597d68a1
Facuud2 <lunariush553@gmail.com>
2025-09-01 19:30:43 -0300

    fix: merge issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,37 @@
-import "./App.css";
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import './App.css';
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
-import InfoAcademica from "./InfoAcademica";
-import ProtectedRoute from "./ProtectedRoute";
-import { AuthProvider } from "./AuthContext";
-import Auth from "./components/Auth";
-import Dashboard from "./Dashboard";
-
+import ProtectedRoute from './ProtectedRoute';
+import { AuthProvider } from './AuthContext';
+import Auth from './components/Auth';
+import Dashboard from './Dashboard';
 
 function AppRoutes() {
-
   return (
     <Routes>
-      <Route path="/" element={
-        <Auth />
-      } />
-      <Route 
-        path="/dashboard" 
+      <Route path="/" element={<Auth />} />
+      <Route
+        path="/dashboard"
         element={
           <ProtectedRoute>
-            <Dashboard/>
+            <Dashboard />
           </ProtectedRoute>
-        } 
+        }
       />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
+  );
 }
 
 function App() {
-
   return (
     <BrowserRouter>
       <AuthProvider>
-      <Toaster position="top-right" />
+        <Toaster position="top-right" />
         <AppRoutes />
       </AuthProvider>
     </BrowserRouter>
   );
-};
+}
 export default App;
