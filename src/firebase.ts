80234ee3d799ca2b7479d8d85544561cdeadbd70
Micaela Cabrera <135764172+MicaelaCabrera@users.noreply.github.com>
2025-09-08 20:43:56 -0300

    cambios Sidebar

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,19 +7,19 @@ import { getStorage } from 'firebase/storage';
 // Tu configuración de Firebase para la aplicación web (la obtienes de la consola de Firebase)
 // ¡IMPORTANTE! Estos valores deben venir de tus variables de entorno (.env)
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Inicializa Firebase
 const app = initializeApp(firebaseConfig);
 
 // Exporta los servicios de Firebase para poder usarlos en otras partes de la aplicación
-// Esto es lo que permite que tu `import { auth } from ...` funcione.
+// Esto es lo que permite que tu import { auth } from ... funcione.
 
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider(); // Si usas Google Sign-In
